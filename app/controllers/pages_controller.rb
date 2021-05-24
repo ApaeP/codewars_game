cbc780936745e4752cb416e7646ad903e5d0460f
Paul Portier <paulportier@gmail.com>
2021-05-24 20:37:25 +0200

    fix home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i(home)
 
   def home
-    @katas = current_user.katas.sort_by { |k| k.level_int.nil? ? -10 : k.level_int }.reverse
+    if current_user
+      @katas = current_user.katas.sort_by { |k| k.level_int.nil? ? -10 : k.level_int }.reverse
+    end
   end
 
   def dashboard
